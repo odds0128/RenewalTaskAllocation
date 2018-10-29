1c66ae0d97097b19b616ff17536996c0e8856477
Ryoya Funato <r.funato@Nuviana.local>
2018-10-29 19:16:12 +0900

    色々忘れていたので追加

--- src/agent/elements/LeaderElements.java
+++ src/agent/elements/LeaderElements.java
@@ -2,9 +2,70 @@ package agent.elements;
 
 import agent.Agent;
 import task.Subtask;
+import task.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static root.EnvironmentalConstants.INITIAL_E_MEMBER;
 
 public class LeaderElements {
-    private Subtask assignedSubtask;
-    private double  DE_m;
-    private Agent leader;
+    private double    e_leader;
+    private double[]  DE_l;
+
+    private Task assignedTask;
+    private List<Agent> candidates;
+    private List<Agent> teammates;
+
+    LeaderElements(int agent_num){
+        this.DE_l = new double[agent_num];
+        this.candidates = new ArrayList<>();
+        this.teammates  = new ArrayList<>();
+
+        this.e_leader = INITIAL_E_MEMBER;
+        for(int i=0; i< agent_num; i++) {
+            this.DE_l[i] = INITIAL_E_MEMBER;
+        }
+    }
+
+    public double getE_leader() {
+        return e_leader;
+    }
+
+    public void setE_leader(double e_leader) {
+        this.e_leader = e_leader;
+    }
+
+    public double[] getDE_l() {
+        return DE_l;
+    }
+
+    public void setDE_l(double[] DE_l) {
+        this.DE_l = DE_l;
+    }
+
+    public Task getAssignedTask() {
+        return assignedTask;
+    }
+
+    public void setAssignedTask(Task assignedTask) {
+        this.assignedTask = assignedTask;
+    }
+
+    public List<Agent> getCandidates() {
+        return candidates;
+    }
+
+    public void setCandidates(List<Agent> candidates) {
+        this.candidates = candidates;
+    }
+
+    public List<Agent> getTeammates() {
+        return teammates;
+    }
+
+    public void setTeammates(List<Agent> teammates) {
+        this.teammates = teammates;
+    }
+
 }
--- src/agent/elements/MemberElements.java
+++ src/agent/elements/MemberElements.java
@@ -2,17 +2,59 @@ package agent.elements;
 
 import agent.Agent;
 import task.Subtask;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static root.EnvironmentalConstants.*;
 
 public class MemberElements  {
-    private double  e_member;
-    private Subtask assignedSubtask;
-    private double  DE_m;
+    private double    e_member;
+    private double[]  DE_m;
+
+    private List<Subtask> assignedSubtask;
     private Agent leader;
 
-    MemberElements(){
+    MemberElements(int agent_num){
+        this.DE_m = new double[agent_num];
+        this.assignedSubtask = new ArrayList<>();
+
         this.e_member = INITIAL_E_MEMBER;
-        this.DE_m = INITIAL_E_MEMBER;  // 他の全エージェントについて繰り返す
+        for(int i=0; i< agent_num; i++) {
+            this.DE_m[i] = INITIAL_E_MEMBER;  // 他の全エージェントについて繰り返す
+        }
     }
 
+
+    public double getE_member() {
+        return e_member;
+    }
+
+    public void setE_member(double e_member) {
+        this.e_member = e_member;
+    }
+
+    public double[] getDE_m() {
+        return DE_m;
+    }
+
+    public void setDE_m(double[] DE_m) {
+        this.DE_m = DE_m;
+    }
+
+    public Agent getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Agent leader) {
+        this.leader = leader;
+    }
+
+    public List<Subtask> getAssignedSubtask() {
+        return assignedSubtask;
+    }
+
+    public void setAssignedSubtask(List<Subtask> assignedSubtask) {
+        this.assignedSubtask = assignedSubtask;
+    }
 }
